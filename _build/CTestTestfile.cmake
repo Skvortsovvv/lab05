b1cd91811bca4f9403b4d864a5410fbdd1464c5b
Skvortsovvv <vladimirs001@yandex.ru>
2020-06-05 12:06:25 +0300

    fixed errors

---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Skvortsovvv/workspace/projects/lab05
+# Build directory: /home/user/Skvortsovvv/workspace/projects/lab05/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/user/Skvortsovvv/workspace/projects/lab05/_build/check")
+subdirs("third-party/gtest")
